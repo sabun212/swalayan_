c6863b0db06a2dc94115992567908517856d8391
Chairunnas Arief Maulana <109496229+sabun212@users.noreply.github.com>
2023-01-18 14:00:27 +0700

    kedua

--- dashboard.php
+++ dashboard.php
@@ -21,6 +21,7 @@ echo "Selamat datang, " . $username . "!";
     <link rel="stylesheet" href="assets/css/main/app-dark.css">
     <link rel="shortcut icon" href="assets/images/logo/favicon.svg" type="image/x-icon">
     <link rel="shortcut icon" href="assets/images/logo/favicon.png" type="image/png">
+    <link rel="icon" type="image/png" href="authstyle/images/icons/favicon.ico" />
 
     <link rel="stylesheet" href="assets/css/shared/iconly.css">
 
--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<?php
+error_reporting(0);
+session_start();
+if ($_SESSION['status'] == "login"){
+	header("location:admin.php");
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
